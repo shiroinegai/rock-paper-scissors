44edfdf790942715f3c6870e2bbc3639758d0db3
Shiroi Negai <shiroinegai@pm.me>
2021-08-16 05:32:49 +0800

    Declare computerPlay function

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Rock Paper Scissors is running.");
+
+let selections = ["rock", "paper", "scissors"];
+
+computerPlay = () => {
+  return selections[Math.floor(Math.random() * 3)];
+};
+
+console.log(computerPlay());
