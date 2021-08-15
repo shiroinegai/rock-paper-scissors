b089cabd003868e51a1501166efecbe84435ae18
Shiroi Negai <shiroinegai@pm.me>
2021-08-16 06:00:47 +0800

    Declare playRound function

--- main.js
+++ main.js
@@ -6,4 +6,20 @@ computerPlay = () => {
   return selections[Math.floor(Math.random() * 3)];
 };
 
-console.log(computerPlay());
+playRound = (playerSelection = "rock", computerSelection = computerPlay()) => {
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    console.log(`You win, ${playerSelection} defeated ${computerSelection}!`);
+  } else if (playerSelection === computerSelection) {
+    console.log(`Both players picked ${computerSelection}, it's a tie!`);
+  } else {
+    console.log(
+      `You lose, ${computerSelection} defeated ${playerSelection}...`
+    );
+  }
+};
+
+playRound();
