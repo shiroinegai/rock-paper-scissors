2c309761a409ef45cd95fda8dae466b8cff2334f
Shiroi Negai <shiroinegai@pm.me>
2021-08-16 06:35:04 +0800

    Add playerSelection prompt and game function

--- main.js
+++ main.js
@@ -6,20 +6,35 @@ computerPlay = () => {
   return selections[Math.floor(Math.random() * 3)];
 };
 
-playRound = (playerSelection = "rock", computerSelection = computerPlay()) => {
-  if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    console.log(`You win, ${playerSelection} defeated ${computerSelection}!`);
-  } else if (playerSelection === computerSelection) {
-    console.log(`Both players picked ${computerSelection}, it's a tie!`);
+playRound = (playerSelection, computerSelection = computerPlay()) => {
+  playerSelection = prompt(
+    "Please enter rock, paper or scissors:"
+  ).toLowerCase();
+  if (selections.includes(playerSelection)) {
+    if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      console.log(`You win, ${playerSelection} defeated ${computerSelection}!`);
+    } else if (playerSelection === computerSelection) {
+      console.log(`Both players picked ${computerSelection}, it's a tie!`);
+    } else {
+      console.log(
+        `You lose, ${computerSelection} defeated ${playerSelection}...`
+      );
+    }
   } else {
-    console.log(
-      `You lose, ${computerSelection} defeated ${playerSelection}...`
-    );
+    console.log("Invalid input, please try again");
   }
 };
 
-playRound();
+game = () => {
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+};
+
+game();
